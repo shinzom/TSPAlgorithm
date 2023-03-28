e979925055dce5a3b48c2dafdcb410577ee28e7a
xyttt <1223340035@qq.com>
2023-03-28 17:17:02 +0800

    修改

---
+++ 后端/src/test/java/com/cy/demo1/mapper/ShowMapperTests.java
@@ -0,0 +1,35 @@
+package com.cy.demo1.mapper;
+
+import com.cy.demo1.entity.Result_;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@SpringBootTest
+//@RunWith：表示启动这个单元测试类
+@RunWith(SpringRunner.class)
+public class ShowMapperTests {
+    @Resource
+    private ShowMapper showMapper;
+
+    @Test
+    public void show() {
+        Integer[] list = showMapper.listId();
+        for(int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + " ");
+        }
+    }
+
+    @Test
+    public void getResult_tx() {
+        Result_ result = showMapper.getResult_tx(4);
+        System.out.println("resultId: " + result.getResult_id());
+        System.out.println("time: " + result.getTime());
+        System.out.println("distance: " + result.getDistance());
+        System.out.println("path: " + result.getPath());
+    }
+
+}
